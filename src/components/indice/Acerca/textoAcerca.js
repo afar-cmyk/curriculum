29156aa01d6b9b5dc8ac462f52ee08f615096072
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-05-30 21:25:40 -0500

    agrega key a la lista del componente textoAcerca

--- src/components/indice/Acerca/textoAcerca.js
+++ src/components/indice/Acerca/textoAcerca.js
@@ -4,7 +4,7 @@ const textoEncabezado = {
   español: {
     encabezado: 'Acerca de mí',
     contenido: [
-      <>
+      <React.Fragment key='acercaEspañol'>
         <p>
           <strong>Diseñador UI/UX</strong>, <strong>editor de videos</strong> y
           <strong> fotógrafo</strong>, programador de software utilizando JAVA y
@@ -17,13 +17,13 @@ const textoEncabezado = {
           aprendizaje con las habilidades que puedan ser requeridas para llevar
           a cabo las tareas encomendadas.
         </p>
-      </>
+      </React.Fragment>
     ]
   },
   ingles: {
     encabezado: 'About Me',
     contenido: [
-      <>
+      <React.Fragment key='acercaIngles'>
         <p>
           <strong>UI/UX designer</strong>, <strong>video editor</strong> and
           <strong> photographer</strong>, software developer using JAVA and
@@ -35,7 +35,7 @@ const textoEncabezado = {
           Intermediate English level, team player, proactive, fast learner with
           the required skills to carry out the given tasks.
         </p>
-      </>
+      </React.Fragment>
     ]
   }
 }
