5bf9de99189642c27bf299cbb29dd6af7750cee2
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-06-06 22:39:24 -0500

    remover codigo sobrante

--- src/components/indice/ColoresContext.jsx
+++ src/components/indice/ColoresContext.jsx
@@ -4,27 +4,6 @@ import Seo from '../Seo'
 const ColoresContext = React.createContext()
 
 export const ColoresProvider = ({ children }) => {
-  // Color principal
-  let colorPrincipal = '#4aabc980'
-
-  // Color principal de las barras
-  let barrasColorPrincipal = '#4abac9b3'
-
-  // Color secundario de las barras
-  let barrasColorSecundario = '#50c5d540'
-
-  // Color para los titulos H2
-  let titulosH2Base = {
-    '& h2': {
-      color: '#cccccc',
-      textDecoration: `underline ${colorPrincipal}`,
-      textUnderlineOffset: '2px',
-      fontSize: { xs: '25px', sm: '30px', lg: '2em' },
-      mt: 0,
-      mb: { xs: '10px', lg: '15px' }
-    }
-  }
-
   // Variable global para el idioma (Español e Ingles)
   const [estadoIdioma, setEstadoIdioma] = React.useState(true)
   const [idiomaActual, setIdiomaActual] = React.useState('español')
@@ -127,13 +106,9 @@ export const ColoresProvider = ({ children }) => {
   return (
     <ColoresContext.Provider
       value={{
-        colorPrincipal,
-        barrasColorPrincipal,
-        barrasColorSecundario,
         manejadorEstadoIdioma,
         idiomaActual,
         estadoIdioma,
-        titulosH2Base,
         esquemaTema,
         temaActual,
         manejadorEstadoTema
