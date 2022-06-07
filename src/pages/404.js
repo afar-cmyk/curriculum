e645b10a126f8341ebf1f02a461d5fe83aa629d0
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-06-07 18:10:24 -0500

    Ajusta el estilo de la pagina 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { ColoresProvider } from '../components/indice/ColoresContext'
 import Pagina404 from '../components/Pagina404'
+import '../styles/global.css'
 
 const NotFoundPage = () => {
   return (
