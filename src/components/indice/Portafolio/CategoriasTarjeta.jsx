cb8e78f745d019cdee5f4bcacefa82582db03bbc
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2022-06-11 23:12:26 -0500

    Crear el componente CategoriasTarjeta

---
+++ src/components/indice/Portafolio/CategoriasTarjeta.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ColoresContext from '../ColoresContext'
+
+const CategoriasTarjeta = (props) => {
+  const { categoria } = props
+
+  const { temaActual, esquemaTema } = React.useContext(ColoresContext)
+
+  const { bordes, tarjetasCategorias } = esquemaTema[temaActual]
+
+  let envolturaCategorias = {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '0.8em',
+    marginLeft: '1rem'
+  }
+
+  let contenedorCategoria = {
+    borderRadius: '0.3em',
+    backgroundColor: tarjetasCategorias,
+    width: 'fit-content',
+    paddingLeft: '0.5em',
+    paddingRight: '0.5em',
+    border: `solid 1px ${bordes}`,
+    fontSize: '0.8em'
+  }
+
+  return (
+    <div style={{ ...envolturaCategorias }}>
+      <div style={{ ...contenedorCategoria }}>{categoria}</div>
+    </div>
+  )
+}
+
+export default CategoriasTarjeta
--- src/components/indice/Portafolio/InfoTarjeta.jsx
+++ src/components/indice/Portafolio/InfoTarjeta.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ColoresContext from '../ColoresContext'
+import CategoriasTarjeta from './CategoriasTarjeta'
 
 const InfoTarjeta = (props) => {
   const { titulo, categorias } = props
   const { temaActual, esquemaTema } = React.useContext(ColoresContext)
 
-  const { bordes, tarjetas, tarjetasCategorias } = esquemaTema[temaActual]
+  const { tarjetas } = esquemaTema[temaActual]
 
   let tarjetaInfo = {
     width: '100%',
@@ -20,30 +21,13 @@ const InfoTarjeta = (props) => {
     justifyContent: 'center'
   }
 
-  let contenedorCategoria = {
-    borderRadius: '0.3em',
-    backgroundColor: tarjetasCategorias,
-    width: 'fit-content',
-    paddingLeft: '0.5em',
-    paddingRight: '0.5em',
-    border: `solid 1px ${bordes}`,
-    fontSize: '0.8em'
-  }
-
-  let envolturaCategorias = {
-    display: 'flex',
-    flexDirection: 'row',
-    gap: '0.8em',
-    marginLeft: '1rem'
-  }
-
   return (
     <div className='info' style={{ ...tarjetaInfo }}>
       <h3>{titulo}</h3>
-      <div style={{ ...envolturaCategorias }}>
-        <div style={{ ...contenedorCategoria }}>Desarrollo</div>
-        <div style={{ ...contenedorCategoria }}>Diseño</div>
-      </div>
+      {categorias.map((datos) => {
+        const { textoCategoria } = datos
+        return <CategoriasTarjeta categoria={textoCategoria} />
+      })}
     </div>
   )
 }
